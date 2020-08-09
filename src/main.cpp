4a506991624d06dcbda22cd78bf5f69ea27de006
truongkutetk97 <lenhattruong.tk@gmail.com>
2020-08-09 18:04:55 +0700

    Initialize commit of Downloader Tool

---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <thread>
+#include <iostream>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <sys/un.h>
+#include <memory>
+#include <string>
+#include <vector>
+//cURL library
+#include <curl/curl.h>
+//spdlog library
+#include <spdlog/spdlog.h>
+
+#define SERVER_PATH     "/tmp/server.dat"
+#define BUFFER_LENGTH    250
+#define FALSE              0
+#define MAX_PARALLEL 10  
+#define NUM_URLS sizeof(urls)/sizeof(char *)
+ 
+using namespace std;
+
+void test(string context){
+
+}
+
+static size_t write_cb(char *data, size_t n, size_t l, void *userp)
+{
+  /* take care of the data here, ignored in this example */ 
+  (void)data;
+  (void)userp;
+  return n*l;
+}
+ 
+static void add_transfer(CURLM *cm, int i)
+{
+  CURL *eh = curl_easy_init();
+  curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, write_cb);
+  curl_easy_setopt(eh, CURLOPT_URL, urls[i]);
+  curl_easy_setopt(eh, CURLOPT_PRIVATE, urls[i]);
+  curl_multi_add_handle(cm, eh);
+}
+ 
+
+class threadObj{
+  public: 
+    void operator()(int x){
+      for (int i =0; i< x; i++){
+        cout <<i;
+      }
+    }
+};
+int main(int argc, char **argv)
+{
+  cout<<"---Start" <<"\n";
+  int    sd=-1, sd2=-1;
+  int    rc, length;
+  char   buffer[BUFFER_LENGTH];
+  struct sockaddr_un serveraddr;
+  
+
+  CURLM *cm;
+  CURLMsg *msg;
+  unsigned int transfers = 0;
+  int msgs_left = -1;
+  int still_alive = 1;
+ 
+  curl_global_init(CURL_GLOBAL_ALL);
+  cm = curl_multi_init();
+
+/* Limit the amount of simultaneous connections curl should allow: */ 
+  curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, (long)MAX_PARALLEL);
+ 
+  for(transfers = 0; transfers < MAX_PARALLEL; transfers++)
+    add_transfer(cm, transfers);
+ 
+  do {
+    curl_multi_perform(cm, &still_alive);
+ 
+    while((msg = curl_multi_info_read(cm, &msgs_left))) {
+      if(msg->msg == CURLMSG_DONE) {
+        char *url;
+        CURL *e = msg->easy_handle;
+        curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &url);
+        fprintf(stderr, "R: %d - %s <%s>\n",
+                msg->data.result, curl_easy_strerror(msg->data.result), url);
+        curl_multi_remove_handle(cm, e);
+        curl_easy_cleanup(e);
+      }
+      else {
+        fprintf(stderr, "E: CURLMsg (%d)\n", msg->msg);
+      }
+      if(transfers < NUM_URLS)
+        add_transfer(cm, transfers++);
+    }
+    if(still_alive)
+      curl_multi_wait(cm, NULL, 0, 1000, NULL);
+ 
+  } while(still_alive || (transfers < NUM_URLS));
+ 
+  curl_multi_cleanup(cm);
+  curl_global_cleanup();
+  
+  do{
+
+  }while(FALSE);
+  if (sd != -1) close(sd);
+  if (sd2 != -1) close(sd2);
+  unlink(SERVER_PATH);
+  cout<<"truongdeptrai" <<"\n";
+  return 0;
+}
